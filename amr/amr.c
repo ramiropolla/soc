91f9e955fb9020a55052fca4e63dcd587f6f85e0
superdump <superdump@66570020-b32e-0410-962d-e44e8f9afd43>
2006-06-13 13:49:06 +0000

    Stripped out amr.c and built on it from the API guides. Reimplemented the decoding of the bitstream to amr parameters.

---
+++ amr/amr.c
@@ -0,0 +1,213 @@
+/*
+ * AMR audio codec
+ * Copyright (c) 2006 Robert Swain
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+/**
+ * @file amr.c
+ * AMR codec.
+ */
+
+
+#include "amrdata.h"
+#include "avcodec.h"
+#include "bitstream.h"
+
+// #define DEBUG_BITSTREAM
+
+typedef struct AMRContext {
+
+  GetBitContext                        gb;
+  float*                    sample_buffer;
+
+} AMRContext;
+
+
+static int amr_nb_decode_init(AVCodecContext *avctx) {
+
+  AMRContext *p = avctx->priv_data;
+
+  /* We also need to allocate a sample buffer */
+  p->sample_buffer = av_mallocz(sizeof(float)*1024);  // here we used av_mallocz instead of av_malloc
+                                                      // av_mallocz memsets the whole buffer to 0
+
+  /* Check if the allocation was successful */
+  if(p->sample_buffer == NULL)
+    return -1;
+
+  /* return 0 for a successful init, -1 for failure */
+  return 0;
+}
+
+
+static int amr_nb_decode_frame(AVCodecContext *avctx,
+    void *data, int *data_size, uint8_t *buf, int buf_size) {
+
+  AMRContext *p = avctx->priv_data;
+  int16_t *outbuffer = data; // FIXME check possible output data type(s)
+  int i;
+  int16_t amr_prms, q_bit;
+
+  enum RXFrameType frame_type;
+  enum Mode mode, speech_mode;
+
+#ifdef DEBUG_BITSTREAM
+  init_get_bits(&p->gb, buf, buf_size*8);
+  av_log(NULL, AV_LOG_ERROR, "\n\n\nBits from one frame (%d):\n", buf_size*8);
+  for(i=0; i<buf_size*8; i++) {
+    av_log(NULL, AV_LOG_ERROR, "%d",get_bits1(&p->gb));
+  }
+  av_log(NULL, AV_LOG_ERROR, "\n\n\n");
+
+  return -1;
+#endif // DEBUG_BITSTREAM
+
+  // decode the bitstream to amr parameters
+  mode = decode_bitstream(&amr_prms, buf, &frame_type, &speech_mode, &q_bit);
+
+  /* To make it easy the stream can only be 16 bits mono, so let's convert it to that */
+  for (i=0 ; i<buf_size; i++)
+    outbuffer[i] = (int16_t)p->sample_buffer[i]; // FIXME check possible output data type(s)
+
+  /* Report how many samples we got */
+  *data_size = buf_size;
+
+  /* Return the amount of bytes consumed if everything was ok */
+  return *data_size*sizeof(int16_t);
+}
+
+
+static int amr_nb_decode_close(AVCodecContext *avctx) {
+
+  AMRContext *p = avctx->priv_data;
+
+  /* Free allocated memory buffer */
+  av_free(p->sample_buffer);
+
+  /* Return 0 if everything is ok, -1 if not */
+  return 0;
+}
+
+/**
+ * Decode the bitstream into the AMR parameters and discover the frame mode
+ *
+ * @param amr_prms          pointer to the AMR parameters
+ * @param buf               pointer to the input buffer
+ * @param frame_type        pointer to the frame type
+ * @param speech_mode       pointer to the speech mode
+ * @param q_bit             pointer to q_bit which is used to decide if the frame
+ *                          is good or bad
+ *
+ * @return Returns the frame mode
+ */
+
+enum Mode decode_bitstream(int16_t *amr_prms, uint8_t *buf, enum RXFrameType
+    *frame_type, enum Mode *speech_mode, int16_t *q_bit) {
+
+  enum Mode mode;
+  int i;
+  int16_t *mask;
+
+  memset(amr_prms, 0, PRMS_MODE_122 << 1);
+  *q_bit = 0x01 & (*buf >> 2); // FIXME rename q_bit to something more meaningful when i understand what it is
+  mode = 0x0F & (*buf >> 3);
+  buf++;
+
+  switch(mode) {
+    case MODE_DTX:
+      mask = order_MODE_DTX;
+      *frame_type = RX_SID_FIRST; // get SID type bit
+      if(*buf & 0x80) // use the update if there is one
+        *frame_type = RX_SID_UPDATE;
+      *speech_mode = (*buf >> 4) && 0x07; // speech mode indicator
+      break;
+    case 15:
+      *frame_type = RX_NO_DATA;
+      break;
+    case MODE_475:
+      mask = order_MODE_475;
+      *frame_type = RX_SPEECH_GOOD;
+      break;
+    case MODE_515:
+      mask = order_MODE_515;
+      *frame_type = RX_SPEECH_GOOD;
+      break;
+    case MODE_59:
+      mask = order_MODE_59;
+      *frame_type = RX_SPEECH_GOOD;
+      break;
+    case MODE_67:
+      mask = order_MODE_67;
+      *frame_type = RX_SPEECH_GOOD;
+      break;
+    case MODE_74:
+      mask = order_MODE_74;
+      *frame_type = RX_SPEECH_GOOD;
+      break;
+    case MODE_795:
+      mask = order_MODE_795;
+      *frame_type = RX_SPEECH_GOOD;
+      break;
+    case MODE_102:
+      mask = order_MODE_102;
+      *frame_type = RX_SPEECH_GOOD;
+      break;
+    case MODE_122:
+      mask = order_MODE_122;
+      *frame_type = RX_SPEECH_GOOD;
+      break;
+    default:
+      *frame_type = RX_SPEECH_BAD;
+      break;
+  }
+
+  if( (mode == MODE_DTX) ||
+       (mode == MODE_475) ||
+       (mode == MODE_515) ||
+       (mode == MODE_59)  ||
+       (mode == MODE_67)  ||
+       (mode == MODE_74)  ||
+       (mode == MODE_795) ||
+       (mode == MODE_102) ||
+       (mode == MODE_122) ) {
+
+    for(i=1; i<mode_bits[mode]; i++) {
+      if(*buf & 0x80)
+        amr_prms[*mask] = (short)(amr_prms[*mask] + mask[1]);
+      mask += 2;
+      if(i%8)
+        *buf <<= 1;
+      else
+        buf++;
+    }
+  }
+
+
+  return mode;
+}
+
+AVCodec amr_nb_decoder =
+{
+  .name = "amr_nb",
+  .type = CODEC_TYPE_AUDIO,
+  .id = CODEC_ID_AMR_NB,
+  .priv_data_size = sizeof(AMRContext),
+  .init = amr_nb_decode_init,
+  .close = amr_nb_decode_close,
+  .decode = amr_nb_decode_frame,
+};
