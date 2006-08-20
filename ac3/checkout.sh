422332631200a3c8f7bdc1c8aae42f5c15744588
Benjamin Larsson <merbanan@users.noreply.github.com>
2006-08-20 18:39:42 +0000

    Added a simple way to test the decoder with ffmpeg.

--- ac3/checkout.sh
+++ ac3/checkout.sh
@@ -1,1 +1,8 @@
-svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg -r {20060531}
+echo "checking out ffmpeg svn"
+svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg
+echo "copying the ac3 decoder files to ffmpeg/libavcodec"
+cp ac3_decoder.c ffmpeg/libavcodec/
+cp ac3_decoder.h ffmpeg/libavcodec/
+echo "patching ffmpeg"
+cd ffmpeg ; patch -p0 <../lgpl_ac3_decoder.patch ; cd ..
+echo "Done, now just do a regular configure and make to build."
