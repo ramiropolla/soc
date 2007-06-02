f3754cbe6c67faf6c84183d6d6a1e63a82b308a3
Benjamin Larsson <merbanan@users.noreply.github.com>
2007-06-02 16:04:52 +0000

    Missed define.

--- aac/aactab.h
+++ aac/aactab.h
@@ -1,3 +1,6 @@
+//tns
+#define TNS_MAX_ORDER 20
+
 // sampling table
 static const int sampling_table[] = { 96000, 88200, 64000, 48000, 44100, 32000, 24000, 22050, 16000, 12000, 11025, 8000, 7350 };
 
