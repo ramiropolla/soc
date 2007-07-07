f4d324c06ce5e305eaec92b5d81e1c70d635f40d
k.nowosad <k.nowosad@66570020-b32e-0410-962d-e44e8f9afd43>
2007-07-07 00:00:02 +0000

    changes in bitstream functions

--- jpeg2000/j2kenc.c
+++ jpeg2000/j2kenc.c
@@ -225,7 +225,7 @@ static void put_bits(J2kEncoderContext *s, int val, int n) // TODO: optimize
     while (n-- > 0){
         if (s->bit_index == 8)
         {
-            s->bit_index = *s->buf == 0xff ? 1:0;
+            s->bit_index = *s->buf == 0xff;
             *(++s->buf) = 0;
         }
         *s->buf |= val << (7 - s->bit_index++);
@@ -236,7 +236,7 @@ static void put_bits(J2kEncoderContext *s, int val, int n) // TODO: optimize
 static void put_num(J2kEncoderContext *s, int num, int n)
 {
     while(--n >= 0)
-        put_bits(s, (num & (1<<n)) ? 1:0, 1);
+        put_bits(s, (num >> n) & 1, 1);
 }
 
 /* flush the bitstream */
