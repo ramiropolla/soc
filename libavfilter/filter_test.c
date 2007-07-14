ea243a05bddc5510ab7728bba4bd07fdbcb75d7e
koorogi <koorogi@66570020-b32e-0410-962d-e44e8f9afd43>
2007-07-14 19:33:28 +0000

    Allow giving filter instances names, in anticipation of the upcoming AVFilterGraph structure and related functions.

--- libavfilter/filter_test.c
+++ libavfilter/filter_test.c
@@ -43,7 +43,7 @@ AVFilterContext *create_filter(char *argv)
     av_log(NULL, AV_LOG_INFO, "creating filter \"%s\" with args \"%s\"\n",
            name, args ? args : "(none)");
 
-    if((ret = avfilter_create_by_name(name))) {
+    if((ret = avfilter_create_by_name(name, NULL))) {
         if(avfilter_init_filter(ret, args)) {
             av_log(NULL, AV_LOG_ERROR, "error initializing filter!\n");
             avfilter_destroy(ret);
