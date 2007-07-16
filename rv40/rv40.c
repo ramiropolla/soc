80d5a61eac510cf8281cebe6f2c8459f3d120540
kostya <kostya@66570020-b32e-0410-962d-e44e8f9afd43>
2007-07-16 12:15:04 +0000

    DC should be set after dequantizing

--- rv40/rv40.c
+++ rv40/rv40.c
@@ -673,9 +673,9 @@ static int rv40_decode_macroblock(RV40DecContext *r, int *intra_types)
         if(cbp & 1)
             rv40_decode_block(s->block[blknum] + blkoff, gb, r->cur_vlcs, luma_vlc, 0);
         if((cbp & 1) || is16){
+            rv40_dequant4x4(s->block[blknum], blkoff, rv40_qscale_tab[r->quant],rv40_qscale_tab[r->quant]);
             if(is16) //FIXME: optimize
                 s->block[blknum][blkoff] = block16[i];
-            rv40_dequant4x4(s->block[blknum], blkoff, rv40_qscale_tab[r->quant],rv40_qscale_tab[r->quant]);
             rv40_intra_inv_transform(s->block[blknum], blkoff);
         }
     }
