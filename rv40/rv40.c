b8dcbdcd14dc845fcd455199105e3426d4c66bb6
kostya <kostya@66570020-b32e-0410-962d-e44e8f9afd43>
2007-07-17 17:14:59 +0000

    Use luma and chroma quantizer tables in dequantizing

--- rv40/rv40.c
+++ rv40/rv40.c
@@ -675,7 +675,7 @@ static int rv40_decode_macroblock(RV40DecContext *r, int *intra_types)
         if(cbp & 1)
             rv40_decode_block(s->block[blknum] + blkoff, gb, r->cur_vlcs, luma_vlc, 0);
         if((cbp & 1) || is16){
-            rv40_dequant4x4(s->block[blknum], blkoff, rv40_qscale_tab[r->quant],rv40_qscale_tab[r->quant]);
+            rv40_dequant4x4(s->block[blknum], blkoff, rv40_qscale_tab[rv40_luma_quant[0][r->quant]],rv40_qscale_tab[rv40_luma_quant[0][r->quant]]);
             if(is16) //FIXME: optimize
                 s->block[blknum][blkoff] = block16[(i & 3) | ((i & 0xC) << 1)];
             rv40_intra_inv_transform(s->block[blknum], blkoff);
@@ -686,7 +686,7 @@ static int rv40_decode_macroblock(RV40DecContext *r, int *intra_types)
         blknum = ((i & 4) >> 2) + 4;
         blkoff = ((i & 1) << 2) + ((i & 2) << 4);
         rv40_decode_block(s->block[blknum] + blkoff, gb, r->cur_vlcs, chroma_vlc, 1);
-        rv40_dequant4x4(s->block[blknum], blkoff, rv40_qscale_tab[r->quant],rv40_qscale_tab[r->quant]);
+        rv40_dequant4x4(s->block[blknum], blkoff, rv40_qscale_tab[rv40_chroma_quant[0][r->quant]],rv40_chroma_quant[0][rv40_qscale_tab[r->quant]]);
         rv40_intra_inv_transform(s->block[blknum], blkoff);
     }
     rv40_output_macroblock(r, intra_types, cbp2, is16);
