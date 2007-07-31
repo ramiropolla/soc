b6d1a2c9f79c4422ea923c51e9f8669658ef79f1
realsun <realsun@66570020-b32e-0410-962d-e44e8f9afd43>
2007-07-31 02:05:47 +0000

    add user supplied mux_rate

--- dvbmuxer/muxer_dev/mpegtsenc.c
+++ dvbmuxer/muxer_dev/mpegtsenc.c
@@ -460,12 +460,16 @@ static int mpegts_write_header(AVFormatContext *s)
         bitrate += st->codec->bit_rate;
     }
 
+    if(s->mux_rate) {
+        ts->mux_rate= (s->mux_rate + (8 * 50) - 1) / (8 * 50);
+    } else {
     //bitrate += bitrate*5/100;
     bitrate += bitrate * 25 / (8 *  DEFAULT_PES_PAYLOAD_SIZE) +  /* PES header size */
                bitrate * 4 / (8 * TS_PACKET_SIZE) +             /* TS  header size */
                500 * 12 +                                       /* SDT size */
                100 * 16;                                        /* PAT size */
     ts->mux_rate = bitrate;
+    }
     ts->last_pcr = 10000;  /* add a preload value */
 
     service->pcr_packet_freq = (ts->mux_rate * PCR_RETRANS_TIME) /
