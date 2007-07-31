7f6bce5ad1545a589472b093a8de59cd814de2b3
realsun <realsun@66570020-b32e-0410-962d-e44e8f9afd43>
2007-07-31 02:13:06 +0000

    indentation change

--- dvbmuxer/muxer_dev/mpegtsenc.c
+++ dvbmuxer/muxer_dev/mpegtsenc.c
@@ -463,12 +463,12 @@ static int mpegts_write_header(AVFormatContext *s)
     if(s->mux_rate) {
         ts->mux_rate= (s->mux_rate + (8 * 50) - 1) / (8 * 50);
     } else {
-    //bitrate += bitrate*5/100;
-    bitrate += bitrate * 25 / (8 *  DEFAULT_PES_PAYLOAD_SIZE) +  /* PES header size */
-               bitrate * 4 / (8 * TS_PACKET_SIZE) +             /* TS  header size */
-               500 * 12 +                                       /* SDT size */
-               100 * 16;                                        /* PAT size */
-    ts->mux_rate = bitrate;
+        //bitrate += bitrate*5/100;
+        bitrate += bitrate * 25 / (8 *  DEFAULT_PES_PAYLOAD_SIZE) +  /* PES header size */
+                   bitrate * 4 / (8 * TS_PACKET_SIZE) +             /* TS  header size */
+                   500 * 12 +                                       /* SDT size */
+                   100 * 16;                                        /* PAT size */
+        ts->mux_rate = bitrate;
     }
     ts->last_pcr = 10000;  /* add a preload value */
 
