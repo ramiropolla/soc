c5672f9345e872fc09043a1f7fdd310a4dba5081
superdump <superdump@66570020-b32e-0410-962d-e44e8f9afd43>
2007-11-28 22:57:38 +0000

    Useful script for reformatting data tables. Could be made more flexible and easier to use.

---
+++ amr/reformat.pl
@@ -0,0 +1,29 @@
+$grp = 0;
+$mod = 8;
+
+while(<>) {
+    for(split(",")) {
+        /(-?\d*)\.(\d*)F/ or next;
+        push @ints, $1;
+        push @fracs, $2;
+    }
+}
+
+$ilen = (sort {$b <=> $a} map {length} @ints )[0];
+$flen = (sort {$b <=> $a} map {length} @fracs)[0];
+
+for($t = 0; $t <= $#fracs; $t++){
+    if($grp>0 && !($t%$grp)) {
+        printf "{";
+    }
+    printf "%${ilen}s.%-${flen}s", $ints[$t], $fracs[$t];
+    if($grp>0 && $t!=0 && !(($t+1)%$grp)) {
+        printf "}";
+    }
+    if(!(($t+1)%$mod)) {
+        printf ",\n";
+    }else {
+        printf ", ";
+    }
+}
+printf "\n";
