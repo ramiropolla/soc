b198165de07752f278eb9632b6a378ca39db2e06
jbr <jbr@66570020-b32e-0410-962d-e44e8f9afd43>
2007-12-31 01:08:31 +0000

    add ac3_parser.c to soc svn

--- eac3/checkout.sh
+++ eac3/checkout.sh
@@ -1,7 +1,7 @@
-FILES="ac3.c ac3.h ac3tab.c ac3tab.h eac3dec.c ac3dec.c ac3dec.h"
+FILES="ac3.c ac3.h ac3tab.c ac3tab.h eac3dec.c ac3dec.c ac3dec.h ac3_parser.c"
 
 echo "checking out ffmpeg svn"
-for i in $FILES ac3_parser.c Makefile aac_ac3_parser.c aac_ac3_parser.h aac_parser.c allcodecs.c avcodec.h ../libavformat/allformats.h ../libavformat/raw.c allcodecs.h ac3enc.c ../ffmpeg.c; do
+for i in $FILES Makefile aac_ac3_parser.c aac_ac3_parser.h aac_parser.c allcodecs.c avcodec.h ../libavformat/allformats.h ../libavformat/raw.c allcodecs.h ac3enc.c ../ffmpeg.c; do
     rm -f ffmpeg/libavcodec/$i
 done
 svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg -r 11360
