f54e48a23ab79cb2a74eb04c4f075315e46d7deb
jbr <jbr@66570020-b32e-0410-962d-e44e8f9afd43>
2008-01-09 23:25:27 +0000

    remove ac3.c from soc svn and add it to ffmpeg.patch

--- eac3/checkout.sh
+++ eac3/checkout.sh
@@ -1,10 +1,10 @@
-FILES="ac3.c eac3dec.c ac3dec.c ac3dec.h ac3dec_data.c ac3dec_data.h"
+FILES="eac3dec.c ac3dec.c ac3dec.h ac3dec_data.c ac3dec_data.h"
 
 echo "checking out ffmpeg svn"
-for i in $FILES Makefile ac3_parser.c ac3enc.c ac3.h; do
+for i in $FILES Makefile ac3_parser.c ac3enc.c ac3.c ac3.h; do
     rm -f ffmpeg/libavcodec/$i
 done
-svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg -r 11479
+svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg -r 11483
 echo "patching ffmpeg"
 cd ffmpeg
 patch -p0 <../ffmpeg.patch
