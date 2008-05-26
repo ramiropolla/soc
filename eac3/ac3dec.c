ed1de14c64df0317658c53a369f0ce1633730e96
jbr <jbr@66570020-b32e-0410-962d-e44e8f9afd43>
2008-05-26 23:55:55 +0000

    cosmetics: line break

--- eac3/ac3dec.c
+++ eac3/ac3dec.c
@@ -1142,7 +1142,8 @@ static int decode_audio_block(AC3DecodeContext *s, int blk)
 /**
  * Decode a single AC-3 frame.
  */
-static int ac3_decode_frame(AVCodecContext * avctx, void *data, int *data_size, const uint8_t *buf, int buf_size)
+static int ac3_decode_frame(AVCodecContext * avctx, void *data, int *data_size,
+                            const uint8_t *buf, int buf_size)
 {
     AC3DecodeContext *s = avctx->priv_data;
     int16_t *out_samples = (int16_t *)data;
