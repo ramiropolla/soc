103089fc057702eaea3d4657836405ed0810e4e4
jai_menon <jai_menon@66570020-b32e-0410-962d-e44e8f9afd43>
2008-05-30 13:13:39 +0000

    added a simple checkout script

---
+++ alacenc/checkout.sh
@@ -0,0 +1,11 @@
+echo "checking out ffmpeg svn"
+rm -f ffmpeg/libavcodec/alacenc.c
+
+svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg -r 13552
+echo "patching ffmpeg"
+cd ffmpeg
+patch -p0 <../alacenc_build.patch
+echo "copying the ALAC encoder source file to ffmpeg/libavcodec"
+rm -f libavcodec/alacenc.c
+ln -s ../alacenc.c libavcodec/alacenc.c
+echo "Done, now just do a regular configure and make to build."
