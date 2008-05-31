f5325f039fa75594abc2e82e09b7cce72de28603
kostya <kostya@66570020-b32e-0410-962d-e44e8f9afd43>
2008-05-31 08:11:18 +0000

    Hier ist der Drachen.
    Decoder sceleton which can produce dummy AAC files.

---
+++ aacenc/aacenc.c
@@ -0,0 +1,101 @@
+/*
+ * AAC encoder
+ * Copyright (C) 2008 Konstantin Shishkov
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file aacenc.c
+ * AAC encoder
+ */
+
+#include "avcodec.h"
+#include "bitstream.h"
+#include "dsputil.h"
+#include "mpeg4audio.h"
+
+typedef struct {
+    PutBitContext pb;
+    MDCTContext mdct;
+    DECLARE_ALIGNED_16(float, kbd_long_1024[1024]);
+} AACEncContext;
+
+/**
+ * Make AAC audio config object.
+ * @see 1.6.2.1
+ */
+static int put_audio_specific_config(AVCodecContext *avctx)
+{
+    PutBitContext pb;
+    init_put_bits(&pb, avctx->extradata, avctx->extradata_size*8);
+    put_bits(&pb, 5, 2); //object type - AAC-LC
+    for(i = 0; i < 16; i++)
+        if(avctx->sample_rate == ff_mpeg4audio_sample_rates[i])
+            break;
+    if(i == 16){
+        av_log(avctx, AV_LOG_ERROR, "Unsupported sample rate %d\n", avctx->sample_rate);
+        return -1;
+    }
+    put_bits(&pb, 4, i); //sample rate index
+    put_bits(&pb, 4, avctx->channels); //channel config - stereo
+    //GASpecificConfig
+    put_bits(&pb, 1, 0); //frame length - 1024 samples
+    put_bits(&pb, 1, 0); //does not depend on core coder
+    put_bits(&pb, 1, 0); //is not extension
+    flush_put_bits(&pb);
+}
+
+static int aac_encode_init(AVCodecContext *avctx)
+{
+    AACEncContext *c = avctx->priv_data;
+    int i;
+
+    avctx->frame_size = 1024;
+
+    ff_mdct_init(&c->mdct, 11, 1);
+    // window init
+    ff_kbd_window_init(c->kbd_long_1024, 4.0, 1024);
+
+    avctx->extradata = av_malloc(2);
+    avctx->extradata_size = 2;
+    put_audio_specific_config(avctx);
+    return 0;
+}
+
+static int aac_encode_frame(AVCodecContext *avctx,
+                            uint8_t *frame, int buf_size, void *data)
+{
+    int i,k,channel;
+    AACEncContext *s = avctx->priv_data;
+    int16_t *samples = data;
+
+    init_put_bits(&s->pb, frame, buf_size*8);
+    put_bits(&s->pb, 8, 0xAB);
+
+    flush_put_bits(&s->pb);
+    return put_bits_count(&s->pb)>>3;
+}
+
+AVCodec aac_encoder = {
+    "aac",
+    CODEC_TYPE_AUDIO,
+    CODEC_ID_AAC,
+    sizeof(AACEncContext),
+    aac_encode_init,
+    aac_encode_frame,
+};
