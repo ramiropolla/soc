57e614f665b100e243fb5292dcbef2e585de82cd
jai_menon <jai_menon@66570020-b32e-0410-962d-e44e8f9afd43>
2008-06-08 18:59:50 +0000

    fix checkout script

--- alacenc/checkout.sh
+++ alacenc/checkout.sh
@@ -7,5 +7,5 @@ cd ffmpeg
 patch -p0 <../alacenc_build.patch
 echo "copying the ALAC encoder source file to ffmpeg/libavcodec"
 rm -f libavcodec/alacenc.c
-ln -s ../alacenc.c libavcodec/alacenc.c
+ln -s ../../alacenc.c libavcodec/alacenc.c
 echo "Done, now just do a regular configure and make to build."
