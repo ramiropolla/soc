1258fc39b290e2b080a179993cb937aa3c9b2213
superdump <superdump@66570020-b32e-0410-962d-e44e8f9afd43>
2008-06-30 09:06:40 +0000

    Add checkout script to make life easier for peoples

---
+++ aacenc/checkout.sh
@@ -0,0 +1,18 @@
+echo "Checking out FFmpeg SVN trunk code..."
+svn co svn://svn.mplayerhq.hu/ffmpeg/trunk ffmpeg && \
+cd ffmpeg
+
+echo "Patching build system"
+patch -p1 < ../aac_enc.patch && \
+cd libavcodec
+
+echo "Copying and downloading source code to libavcodec dir"
+cp ../../aacenc.c .
+cp ../../aacpsy.c .
+cp ../../aacpsy.h .
+
+cd ../..
+svn co svn://svn.mplayerhq.hu/soc/aac
+cp aac/aactab.h ffmpeg/libavcodec/.
+
+echo "Done! Now enter the ffmpeg dir, configure and make FFmpeg and enjoy the AAC encoder! :)"
