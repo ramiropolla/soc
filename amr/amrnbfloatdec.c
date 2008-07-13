e2fd3e3820b1da283c6d0a1ff079fa48f869e681
superdump <superdump@66570020-b32e-0410-962d-e44e8f9afd43>
2008-07-13 22:46:25 +0000

    Use the correct set of samples from samples_in[]

--- amr/amrnbfloatdec.c
+++ amr/amrnbfloatdec.c
@@ -1294,10 +1294,10 @@ static int amrnb_decode_frame(AVCodecContext *avctx,
         update_state(p);
 
         // convert float samples to 16-bit integer
-        for(i=0; i<AMR_SUBFRAME_SIZE; i++) {
+        for(i=LP_FILTER_ORDER; i<LP_FILTER_ORDER+AMR_SUBFRAME_SIZE; i++) {
             p->samples_in[i] = p->samples_in[i] * p->mul_bias + p->add_bias;
         }
-        p->dsp.float_to_int16(p->samples_out, p->samples_in, AMR_SUBFRAME_SIZE);
+        p->dsp.float_to_int16(p->samples_out, &p->samples_in[LP_FILTER_ORDER], AMR_SUBFRAME_SIZE);
         memcpy(&buf_out[subframe*AMR_SUBFRAME_SIZE], p->samples_out, AMR_SUBFRAME_SIZE*sizeof(int16_t));
 
     }
