07d22b8f7042a2c45b7fd3af74251b42e27fd0be
superdump <superdump@66570020-b32e-0410-962d-e44e8f9afd43>
2008-07-13 22:35:21 +0000

    Move update_state() so that the samples_in history is in the correct form for the calculations conducted in synthesis()

--- amr/amrnbfloatdec.c
+++ amr/amrnbfloatdec.c
@@ -1290,15 +1290,15 @@ static int amrnb_decode_frame(AVCodecContext *avctx,
 
 /*** end of synthesis ***/
 
+        // update buffers and history
+        update_state(p);
+
         // convert float samples to 16-bit integer
         for(i=0; i<AMR_SUBFRAME_SIZE; i++) {
             p->samples_in[i] = p->samples_in[i] * p->mul_bias + p->add_bias;
         }
         p->dsp.float_to_int16(p->samples_out, p->samples_in, AMR_SUBFRAME_SIZE);
         memcpy(&buf_out[subframe*AMR_SUBFRAME_SIZE], p->samples_out, AMR_SUBFRAME_SIZE*sizeof(int16_t));
-
-        // update buffers and history
-        update_state(p);
 
     }
 
