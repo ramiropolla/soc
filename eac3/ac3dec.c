6913f088148a0e1154020342501158b6805ca76d
jbr <jbr@66570020-b32e-0410-962d-e44e8f9afd43>
2008-08-05 01:33:54 +0000

    cosmetics: change comments and text to refer to block decoding instead of block parsing

--- eac3/ac3dec.c
+++ eac3/ac3dec.c
@@ -1308,10 +1308,10 @@ static int ac3_decode_frame(AVCodecContext * avctx, void *data, int *data_size,
             s->output_mode  = s->out_channels == 1 ? AC3_CHMODE_MONO : AC3_CHMODE_STEREO;
     }
 
-    /* parse the audio blocks */
+    /* decode the audio blocks */
     for (blk = 0; blk < s->num_blocks; blk++) {
         if (!err && decode_audio_block(s, blk)) {
-            av_log(avctx, AV_LOG_ERROR, "error parsing the audio block\n");
+            av_log(avctx, AV_LOG_ERROR, "error decoding the audio block\n");
         }
 
         /* interleave output samples */
