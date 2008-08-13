e2b27e8b0fc74c781f8489e980979256119d9f77
spyfeng <spyfeng@66570020-b32e-0410-962d-e44e8f9afd43>
2008-08-13 02:09:18 +0000

    add conditional compile for mxf.h

--- mxf/mxf.h
+++ mxf/mxf.h
@@ -18,6 +18,8 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+#ifndef FFMPEG_MXF_H
+#define FFMPEG_MXF_H
 
 #include "avformat.h"
 #include "libavcodec/bytestream.h"
@@ -88,3 +90,4 @@ static const MXFCodecUL mxf_codec_uls[] = {
 #define PRINT_KEY(pc, s, x) dprintf(pc, "%s %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X\n", s, \
                              (x)[0], (x)[1], (x)[2], (x)[3], (x)[4], (x)[5], (x)[6], (x)[7], (x)[8], (x)[9], (x)[10], (x)[11], (x)[12], (x)[13], (x)[14], (x)[15])
 
+#endif /* FFMPEG_MXF_H */
