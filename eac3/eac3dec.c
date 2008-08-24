d37d51ae8bc9e5d41afd8e9c076d8caed508362d
jbr <jbr@66570020-b32e-0410-962d-e44e8f9afd43>
2008-08-24 15:31:46 +0000

    output a warning message if the GAQ gain group code is out-of-range

--- eac3/eac3dec.c
+++ eac3/eac3dec.c
@@ -104,7 +104,11 @@ void ff_eac3_get_transform_coeffs_aht_ch(AC3DecodeContext *s, int ch)
         for (bin = s->start_freq[ch]; bin < s->end_freq[ch]; bin++) {
             if (s->bap[ch][bin] > 7 && s->bap[ch][bin] < end_bap) {
                 if (gc++ == 2) {
-                    int group_gain = av_clip(get_bits(gbc, 5), 0, 26);
+                    int group_gain = get_bits(gbc, 5);
+                    if (group_gain > 26) {
+                        av_log(s->avctx, AV_LOG_WARNING, "GAQ gain value out-of-range\n");
+                        group_gain = 26;
+                    }
                     gaq_gain[gs++] = ff_ac3_ungroup_3_in_5_bits_tab[group_gain][0];
                     gaq_gain[gs++] = ff_ac3_ungroup_3_in_5_bits_tab[group_gain][1];
                     gaq_gain[gs++] = ff_ac3_ungroup_3_in_5_bits_tab[group_gain][2];
