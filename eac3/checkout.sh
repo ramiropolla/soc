f644f74d78b69de7e5b58846fc861d36900bcbfd
jbr <jbr@66570020-b32e-0410-962d-e44e8f9afd43>
2008-08-27 01:50:49 +0000

    update to current FFmpeg SVN r14991

--- eac3/checkout.sh
+++ eac3/checkout.sh
@@ -1,10 +1,10 @@
 FILES="eac3dec.c ac3dec.c"
 
 echo "checking out ffmpeg svn"
-for i in $FILES Makefile ac3dec.h ac3dec_data.c; do
+for i in $FILES Makefile; do
     rm -f ffmpeg/libavcodec/$i
 done
-svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg -r 14960
+svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg -r 14991
 echo "patching ffmpeg"
 cd ffmpeg
 patch -p0 <../ffmpeg.patch
