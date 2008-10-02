29cb8573fa765d327f823ec934f5e10fc6001c0d
stefano <stefano@66570020-b32e-0410-962d-e44e8f9afd43>
2008-10-02 19:16:37 +0000

    Add the checkout.sh script and update the README file accordingly.

---
+++ libavfilter/checkout.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+echo "checking out pristine ffmpeg"
+svn checkout svn://svn.mplayerhq.hu/ffmpeg/trunk/ ffmpeg -r15000
+
+echo "patching ffmpeg"
+for diff in $(ls $(pwd)/diffs/*.diff); do patch -d ffmpeg -p0 -i $diff; done
+
+echo "copying files to libavfilter"
+find -maxdepth 1 -type f -not -name $(basename $0) -exec cp {} ffmpeg/libavfilter \;
