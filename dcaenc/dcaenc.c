a29fd5228fe606de666e8eb5913ed0653ee92b74
diego <diego@66570020-b32e-0410-962d-e44e8f9afd43>
2008-10-05 15:33:46 +0000

    Relicense as LGPL v2.1 or later with the author's permission.

--- dcaenc/dcaenc.c
+++ dcaenc/dcaenc.c
@@ -2,9 +2,21 @@
  * DCA encoder
  * Copyright (C) 2008 Alexander E. Patrakov
  *
- * This file is not yet part of FFmpeg.
+ * This file is part of FFmpeg.
  *
- * When this file is part of FFmpeg it can be licensed under the LGPL version 2.1 or later
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 /* FFmpeg port by Benjamin Larsson */
