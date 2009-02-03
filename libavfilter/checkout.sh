2e66b19be887c353ff7aaff6fbcd655d60a3ae85
superdump <superdump@66570020-b32e-0410-962d-e44e8f9afd43>
2009-02-03 23:38:00 +0000

    Non-GNU 'find' seems to require that the first argument be the base search path

--- libavfilter/checkout.sh
+++ libavfilter/checkout.sh
@@ -7,4 +7,4 @@ echo "patching ffmpeg"
 for diff in $(ls $(pwd)/diffs/*.diff); do patch -d ffmpeg -p0 -i $diff; done
 
 echo "copying files to libavfilter"
-find -maxdepth 1 -type f -not -name $(basename $0) -exec cp {} ffmpeg/libavfilter \;
+find $(pwd) -maxdepth 1 -type f -not -name $(basename $0) -exec cp {} ffmpeg/libavfilter \;
