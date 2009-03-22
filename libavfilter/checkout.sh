480b6659a2e715ef888ed38eee86a5706ab123be
victor <victor@66570020-b32e-0410-962d-e44e8f9afd43>
2009-03-22 16:15:05 +0000

    Initial documentation of video filters.

--- libavfilter/checkout.sh
+++ libavfilter/checkout.sh
@@ -8,3 +8,6 @@ for diff in $(ls $(pwd)/diffs/*.diff); do patch -d ffmpeg -p0 -i $diff; done
 
 echo "copying files to libavfilter"
 find $(pwd) -maxdepth 1 -type f -not -name $(basename $0) -exec cp {} ffmpeg/libavfilter \;
+
+echo "copying libavfilter doc files"
+cp -p $(pwd)/doc/* ffmpeg/doc
