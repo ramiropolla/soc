5a5ea20c4f4e54b82fdc473c0ecc98f5805839b6
alexc <alexc@66570020-b32e-0410-962d-e44e8f9afd43>
2009-05-21 17:22:29 +0000

    Cosmetics after last commit

--- aacenc/psymodel.c
+++ aacenc/psymodel.c
@@ -123,9 +123,8 @@ av_cold void ff_psy_preprocess_end(struct FFPsyPreprocessContext *ctx)
     int i;
     ff_iir_filter_free_coeffs(ctx->fcoeffs);
     if (ctx->fstate)
-    for(i = 0; i < ctx->avctx->channels; i++){
-        ff_iir_filter_free_state(ctx->fstate[i]);
-    }
+        for (i = 0; i < ctx->avctx->channels; i++)
+            ff_iir_filter_free_state(ctx->fstate[i]);
     av_freep(&ctx->fstate);
 }
 
