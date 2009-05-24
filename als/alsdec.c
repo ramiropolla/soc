fc68152eba1589896dbffe79a25ccc63dfe814b0
thilo.borgmann <thilo.borgmann@66570020-b32e-0410-962d-e44e8f9afd43>
2009-05-24 01:22:14 +0000

    Initial commit: Adds a README file describing the project. Introduces the ALS decoder main file: alsdec.c. Provides the necessary patch to the ffmpeg tree to merge this decoder with the codebase.

---
+++ als/alsdec.c
@@ -0,0 +1,70 @@
+/*
+ * ALS decoder
+ * Copyright (c) 2009 Thilo Borgmann <thilo.borgmann _at_ googlemail.com>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file libavcodec/alsdec.c
+ * MPEG-4 ALS decoder
+ * @author Thilo Borgmann <thilo.borgmann _at_ googlemail.com>
+ */
+
+
+#include "avcodec.h"
+
+
+typedef struct {
+} ALSDecContext;
+
+
+/** Decode an ALS frame.
+ */
+static int als_decode_frame(AVCodecContext *avctx,
+                            void *data, int *data_size,
+                            AVPacket *avpkt) {
+    return 0;
+}
+
+
+/** Initialize the ALS decoder.
+ */
+static int als_decode_init(AVCodecContext *avctx) {
+    return 0;
+}
+
+
+/** Uninitialize the ALS decoder.
+ */
+static int als_decode_end(AVCodecContext *avctx) {
+    return 0;
+}
+
+
+AVCodec als_decoder = {
+    "als",
+    CODEC_TYPE_AUDIO,
+    CODEC_ID_MP4ALS,
+    sizeof(ALSDecContext),
+    als_decode_init,
+    NULL,
+    als_decode_end,
+    als_decode_frame,
+    .long_name = NULL_IF_CONFIG_SMALL("MPEG-4 Audio Lossless Coding"),
+};
+
