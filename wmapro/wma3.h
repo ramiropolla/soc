e8832e15b9b656e55bc56d5d274bc0e2072f0d57
faust3 <faust3@66570020-b32e-0410-962d-e44e8f9afd43>
2009-05-30 20:39:58 +0000

    Fixed libavcodec/wma3dec.c:746: warning: array subscript has type ‘char’

--- wmapro/wma3.h
+++ wmapro/wma3.h
@@ -74,11 +74,11 @@ typedef struct {
  * @brief channel group for channel transformations
  */
 typedef struct {
-    char num_channels;                                     ///< number of channels in the group
-    char transform;                                        ///< controls the type of the transform
-    char transform_band[MAX_BANDS];                        ///< controls if the transform is enabled for a certain band
-    float decorrelation_matrix[MAX_CHANNELS*MAX_CHANNELS]; ///< decorrelation matrix
-    char use_channel[MAX_CHANNELS];                        ///< channels that are part of the group
+    uint8_t num_channels;                                     ///< number of channels in the group
+    char    transform;                                        ///< controls the type of the transform
+    char    transform_band[MAX_BANDS];                        ///< controls if the transform is enabled for a certain band
+    float   decorrelation_matrix[MAX_CHANNELS*MAX_CHANNELS];  ///< decorrelation matrix
+    char    use_channel[MAX_CHANNELS];                        ///< channels that are part of the group
 } WMA3ChannelGroup;
 
 /**
