aa616b05448971449335d83edc8f71e26b950357
cmcq <cmcq@66570020-b32e-0410-962d-e44e8f9afd43>
2009-06-04 10:56:20 +0000

    decode_pitch_vector must store its result in p->pitch_vector p->pitch_vector is used for synthesis but it was never being set.

--- amr/amrnbfloatdec.c
+++ amr/amrnbfloatdec.c
@@ -492,6 +492,8 @@ static void decode_pitch_vector(AMRContext *p,
     // vector
     interp_pitch_vector(p->excitation, p->pitch_lag_int, pitch_lag_frac,
                         p->cur_frame_mode);
+
+    memcpy(p->pitch_vector, p->excitation, AMR_SUBFRAME_SIZE * sizeof(float));
 }
 /// @}
 
