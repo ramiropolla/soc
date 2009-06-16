ea58e05fb0f101c2db4541450b6c96da128ade70
alexc <alexc@66570020-b32e-0410-962d-e44e8f9afd43>
2009-06-16 17:46:45 +0000

    Update the checkout script. AACenc should now "just work."

--- aacenc/checkout.sh
+++ aacenc/checkout.sh
@@ -11,14 +11,10 @@ cd libavcodec
 echo "Copying and downloading source code to libavcodec dir"
 cp ../../aac.h .
 cp ../../aacenc.c .
+cp ../../aacenc.h .
+cp ../../aaccoder.c .
 cp ../../aacpsy.c .
-cp ../../aacpsy.h .
-cp ../../lowpass.c .
-cp ../../lowpass.h .
-
-cd ../..
-svn co svn://svn.ffmpeg.org/soc/aac
-cp aac/aactab.h ffmpeg/libavcodec/.
-cp aac/aactab.c ffmpeg/libavcodec/
+cp ../../psymodel.c .
+cp ../../psymodel.h .
 
 echo "Done! Now enter the ffmpeg dir, configure and make FFmpeg and enjoy the AAC encoder! :)"
