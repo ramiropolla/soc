fb0ec74005ae69ed59e1a1920a770327d36949b2
spyfeng <spyfeng@66570020-b32e-0410-962d-e44e8f9afd43>
2009-06-30 17:43:58 +0000

    continue read the frame when seek failed.

--- seek_api/seektest/seek_test2.c
+++ seek_api/seektest/seek_test2.c
@@ -78,7 +78,7 @@ int main(int argc, char **argv)
         AVStream *av_uninit(st);
 
         memset(&pkt, 0, sizeof(pkt));
-        if(ret>=0){
+        //if(ret>=0){
             ret= av_read_frame(ic, &pkt);
             printf("ret:%2d", ret);
             if(ret>=0){
@@ -86,7 +86,7 @@ int main(int argc, char **argv)
                 printf(" st:%2d dts:%"PRId64" pts:%"PRId64" pos:%" PRId64 " size:%d flags:%d", pkt.stream_index, pkt.dts/**av_q2d(st->time_base)*/, pkt.pts/**av_q2d(st->time_base)*/, pkt.pos, pkt.size, pkt.flags);
             }
             printf("\n");
-        }
+        //}
 
         if(i>25) break;
 
