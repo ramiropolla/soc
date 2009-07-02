2064b6a6a25ea259f7895d84b0fb3867510f7450
spyfeng <spyfeng@66570020-b32e-0410-962d-e44e8f9afd43>
2009-07-02 17:16:01 +0000

    check the differences with expeted result when run x test_dothack2.sh.

--- seek_api/seektest/test_dothack2.sh
+++ seek_api/seektest/test_dothack2.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 echo "Test dothack.mpg from http://samples.mplayerhq.hu/MPEG2/"
 
-./seektest2 dothack2.mpg
+./seek_test2 dothack2.mpg >> res.txt
 
-echo "Done, please check the returned value according to the outputs."
+#echo "Done, please check the returned value according to the outputs."
+if diff -u -w res.txt dothack2.ref ; then
+    echo
+    echo The output is correct.
+    exit 0
+else
+    echo
+    echo Please check the differences output.
+    exit 1
+fi
