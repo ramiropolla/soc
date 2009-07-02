1d22831d6def5697094bf7bffe7f3cc72f100e01
spyfeng <spyfeng@66570020-b32e-0410-962d-e44e8f9afd43>
2009-07-02 17:46:53 +0000

    change the diff order and output redirection type.

--- seek_api/seektest/test_dothack2.sh
+++ seek_api/seektest/test_dothack2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "Test dothack.mpg from http://samples.mplayerhq.hu/MPEG2/"
 
-./seek_test2 dothack2.mpg >> res.txt
+./seek_test2 dothack2.mpg > res.txt
 
 #echo "Done, please check the returned value according to the outputs."
-if diff -u -w res.txt dothack2.ref ; then
+if diff -u -w dothack2.ref res.txt ; then
     echo
     echo The output is correct.
     exit 0
