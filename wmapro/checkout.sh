6fb36b476b7f3540a82df0fabc3ace3d28beb3d2
faust3 <faust3@66570020-b32e-0410-962d-e44e8f9afd43>
2009-07-04 10:27:38 +0000

    update ffmpeg patch, TODO and checkout script

--- wmapro/checkout.sh
+++ wmapro/checkout.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 svn co svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg
 cd ffmpeg/libavcodec
-ln -s ../../wma3dec.c wma3dec.c
-ln -s ../../wma3data.h wma3data.h
-ln -s ../../wma3.h wma3.h
+ln -s ../../wmaprodec.c wmaprodec.c
+ln -s ../../wmaprodata.h wmaprodata.h
 cd ../
 patch -p0 <../wmapro_ffmpeg.patch
 patch -p0 <../audioframesize.patch
