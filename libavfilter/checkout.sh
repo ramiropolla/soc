94aa596342570d8d66ea29b1afafca6268f6673a
stefano <stefano@66570020-b32e-0410-962d-e44e8f9afd43>
2009-07-05 00:27:14 +0000

    Update to FFmpeg r19346.

--- libavfilter/checkout.sh
+++ libavfilter/checkout.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "checking out pristine ffmpeg"
-svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r19066
+svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r19346
 
 echo "patching ffmpeg"
 for diff in $(ls $(pwd)/diffs/*.diff); do patch -d ffmpeg -p0 -i $diff; done
