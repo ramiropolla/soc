2e4f3c40ab71377ff225945b2e2ce3169b809ab4
kostya <kostya@66570020-b32e-0410-962d-e44e8f9afd43>
2009-07-22 05:36:36 +0000

    SHA-2 is in main SVN now

--- rtmp/checkout.sh
+++ rtmp/checkout.sh
@@ -4,11 +4,6 @@ echo "Checking out FFmpeg SVN trunk code..."
 svn co svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg &&
 cd ffmpeg
 
-echo "Adding SHA-256 support"
-cp libavutil/sha1.c libavutil/sha.c
-cp libavutil/sha1.h libavutil/sha.h
-patch -p0 < ../sha2.patch
-
 echo "Patching build system"
 patch -p1 < ../rtmp.patch &&
 cd libavformat
