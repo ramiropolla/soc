48ffdac7f0140efde83682b0d0d547fed2468b33
cmcq <cmcq@66570020-b32e-0410-962d-e44e8f9afd43>
2009-08-03 19:24:24 +0000

    Remove redundant brackets around -14.0 in #define

--- amr/amrnbdata.h
+++ amr/amrnbdata.h
@@ -2068,7 +2068,7 @@ static const uint8_t gray_decode[8] = { 0, 1, 3, 2, 5, 6, 4, 7 };
 
 // gain tables
 
-#define MIN_ENERGY (-14.0)
+#define MIN_ENERGY -14.0
 
 // scalar quantized pitch gain table for 7.95 and 12.2 kbps modes
 static const float qua_gain_pit[16] = {
