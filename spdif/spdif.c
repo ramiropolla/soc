98e62aacebf81f4b3fd4cf160c811e52ec5f0b1e
bwolowiec <bwolowiec@66570020-b32e-0410-962d-e44e8f9afd43>
2009-08-15 10:22:36 +0000

    use new function instead of swab

--- spdif/spdif.c
+++ spdif/spdif.c
@@ -29,8 +29,6 @@
 #include "libavcodec/ac3.h"
 #include "libavcodec/dca.h"
 #include "libavcodec/aac_parser.h"
-#define __USE_XOPEN
-#include <unistd.h>
 
 #define SYNCWORD1 0xF872
 #define SYNCWORD2 0x4E1F
@@ -62,6 +60,25 @@ typedef struct IEC958Context {
     int buffer_size;
 #endif
 } IEC958Context;
+
+//TODO move to DSP
+static void bswap_buf16(uint16_t *dst, const uint16_t *src, int w){
+    int i;
+
+    for(i=0; i+8<=w; i+=8){
+        dst[i+0]= bswap_16(src[i+0]);
+        dst[i+1]= bswap_16(src[i+1]);
+        dst[i+2]= bswap_16(src[i+2]);
+        dst[i+3]= bswap_16(src[i+3]);
+        dst[i+4]= bswap_16(src[i+4]);
+        dst[i+5]= bswap_16(src[i+5]);
+        dst[i+6]= bswap_16(src[i+6]);
+        dst[i+7]= bswap_16(src[i+7]);
+    }
+    for(;i<w; i++){
+        dst[i+0]= bswap_16(src[i+0]);
+    }
+}
 
 static int spdif_header_ac3(AVFormatContext *s, AVPacket *pkt)
 {
@@ -257,7 +274,7 @@ static int spdif_write_packet(struct AVFormatContext *s, AVPacket *pkt)
         if (!ctx->buffer)
             return AVERROR(ENOMEM);
     }
-    swab(pkt->data, ctx->buffer, pkt->size & ~1);
+    bswap_buf16((uint16_t *)ctx->buffer, (uint16_t *)pkt->data, pkt->size >> 1);
     put_buffer(s->pb, ctx->buffer, pkt->size & ~1);
 #endif
 
