bf87b2792bf308c1221bb5413070c7dab8940c67
vitor <vitor@66570020-b32e-0410-962d-e44e8f9afd43>
2009-08-22 05:43:33 +0000

    Make checkout.sh script download the swscale revision corresponding to the FFmpeg one.

--- libavfilter/checkout.sh
+++ libavfilter/checkout.sh
@@ -3,6 +3,11 @@
 echo "checking out pristine ffmpeg"
 svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r19346
 
+echo "downloading the corresponding version of swscale"
+cd ffmpeg/libswscale
+svn up -r29390
+cd ../..
+
 echo "patching ffmpeg"
 for diff in $(ls $(pwd)/diffs/*.diff); do patch -d ffmpeg -p0 -i $diff; done
 
