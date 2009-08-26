c72f511fb8c95b461d7ac145757c82daa75e86f9
faust3 <faust3@66570020-b32e-0410-962d-e44e8f9afd43>
2009-08-26 21:28:36 +0000

    fix checkout script

--- wmapro/checkout.sh
+++ wmapro/checkout.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 svn co svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg
 cd ffmpeg/libavcodec
+rm -rf wmaprodec.c
+rm -rf wmaprodata.h
 ln -s ../../wmaprodec.c wmaprodec.c
 ln -s ../../wmaprodata.h wmaprodata.h
 cd ../
