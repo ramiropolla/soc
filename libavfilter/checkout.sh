2903de691ae6b9f453049923b5ddea73ccf2f6e0
stefano <stefano@66570020-b32e-0410-962d-e44e8f9afd43>
2009-10-18 10:41:28 +0000

    Update to FFmpeg r20275, in particular remove the vf_null filter, which is now part of the main repo.

--- libavfilter/checkout.sh
+++ libavfilter/checkout.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "checking out pristine ffmpeg"
-svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r20272
+svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r20275
 
 echo "downloading the corresponding version of swscale"
 cd ffmpeg/libswscale
