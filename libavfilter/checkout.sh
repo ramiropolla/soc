f36597d358c952d866a95de552c5e49099783adf
stefano <stefano@66570020-b32e-0410-962d-e44e8f9afd43>
2009-10-21 21:42:18 +0000

    Update to FFmpeg r20344. In particular remove the vf_crop.c filter, since it is in the main repo.

--- libavfilter/checkout.sh
+++ libavfilter/checkout.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "checking out pristine ffmpeg"
-svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r20303
+svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r20344
 
 echo "downloading the corresponding version of swscale"
 cd ffmpeg/libswscale
