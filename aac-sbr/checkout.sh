5d27556a1f89cfea646d39c7ba8d7e708ba157fb
alexc <alexc@66570020-b32e-0410-962d-e44e8f9afd43>
2009-11-16 08:15:48 +0000

    {c,h} is not a valid POSIX glob sequence

--- aac-sbr/checkout.sh
+++ aac-sbr/checkout.sh
@@ -3,7 +3,7 @@
 echo "Checking out FFmpeg..."
 svn co svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg -r 20025
 echo "Copying SBR files into FFmpeg tree..."
-cp aacsbr*.{c,h} ffmpeg/libavcodec
+cp aacsbr*.[ch] ffmpeg/libavcodec
 echo "Applying SBR and build system patch"
 cd ffmpeg; patch -p0 < ../ffmpeg.diff
 echo "All done! Now configure and make FFmpeg"
