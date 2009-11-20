f8b958929c66ad66a17271579fa497b81e1ec2ab
alexc <alexc@66570020-b32e-0410-962d-e44e8f9afd43>
2009-11-20 20:48:49 +0000

    Merge changes from trunk such that ffmpeg.diff cleanly applies.

--- aac-sbr/checkout.sh
+++ aac-sbr/checkout.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Checking out FFmpeg..."
-svn co svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg -r 20025
+svn co svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg -r 20564
 echo "Copying SBR files into FFmpeg tree..."
 cp aacsbr*.[ch] ffmpeg/libavcodec
 echo "Applying SBR and build system patch"
