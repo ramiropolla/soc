5e4a950eac0904dd6acd096a90e278e53a1e1c8e
alexc <alexc@66570020-b32e-0410-962d-e44e8f9afd43>
2009-11-26 21:52:52 +0000

    If SBR's reset procedure fails switch back to pure upsampling mode.

--- aac-sbr/aacsbr.c
+++ aac-sbr/aacsbr.c
@@ -778,9 +778,15 @@ static unsigned int sbr_data(AACContext *ac, SpectralBandReplication *sbr,
 
 static void sbr_reset(AACContext *ac, SpectralBandReplication *sbr)
 {
-    sbr_make_f_master(ac, sbr, sbr->spectrum_params);
-    sbr_make_f_derived(ac, sbr);
+    int err;
+    err = sbr_make_f_master(ac, sbr, sbr->spectrum_params);
+    if (err >= 0)
+        err = sbr_make_f_derived(ac, sbr);
     sbr->reset = 0;
+    if (err < 0) {
+        av_log(ac->avccontext, AV_LOG_ERROR, "SBR reset failed. Switching SBR to pure upsampling mode.\n");
+        sbr->start = 0;
+    }
 }
 
 /**
