c97ef78d1798eaf65f819cad85913067aefc3d48
alexc <alexc@66570020-b32e-0410-962d-e44e8f9afd43>
2009-11-28 03:28:03 +0000

    Keep ffmpeg and libswscale synchronized

--- aac-sbr/checkout.sh
+++ aac-sbr/checkout.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "Checking out FFmpeg..."
-svn co svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg -r 20564
+svn co svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg -r 20564 --ignore-externals
+(cd ffmpeg && svn co svn://svn.ffmpeg.org/mplayer/trunk/libswscale libswscale -r 29932)
 echo "Copying SBR files into FFmpeg tree..."
 cp aacsbr*.[ch] ffmpeg/libavcodec
 echo "Applying SBR and build system patch"
