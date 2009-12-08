488a75c065c618032222b95d378938a75db85558
alexc <alexc@66570020-b32e-0410-962d-e44e8f9afd43>
2009-12-08 22:59:06 +0000

    Simplify noise floor decoding when bs_df_noise = 0.
    The bs_df_noise[l] = 0 case of noise floor decoding is just a simple
    horizontal accumulation. This is analogous to r5495 for envelope
    decoding.

--- aac-sbr/aacsbr.c
+++ aac-sbr/aacsbr.c
@@ -970,12 +970,9 @@ static void sbr_env_noise_floors(SpectralBandReplication *sbr, SBRData *ch_data,
             for (k = 0; k < sbr->n_q; k++)
                 sbr->noise_facs[ch][l + 1][k] = sbr->noise_facs[ch][l][k] + delta * ch_data->bs_data_noise[l][k];
         else {
-            for (k = 0; k < sbr->n_q; k++) {
-                sbr->noise_facs[ch][l + 1][k] = ch_data->bs_data_noise[l][0];
-                for (i = 1; i <= k; i++)
-                    sbr->noise_facs[ch][l + 1][k] += ch_data->bs_data_noise[l][i];
-                sbr->noise_facs[ch][l + 1][k] *= delta;
-            }
+            sbr->noise_facs[ch][l + 1][0] = delta * ch_data->bs_data_noise[l][0];
+            for (k = 1; k < sbr->n_q; k++)
+                sbr->noise_facs[ch][l + 1][k] = sbr->noise_facs[ch][l + 1][k - 1] + delta * ch_data->bs_data_noise[l][i];
         }
     }
 
