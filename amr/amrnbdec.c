ff613f65c6fbac9f2a61b3db7f34df5eeeaf2f04
vitor <vitor@66570020-b32e-0410-962d-e44e8f9afd43>
2010-01-01 19:50:23 +0000

    Instead of dividing by a constant, multiply by its inverse.

--- amr/amrnbdec.c
+++ amr/amrnbdec.c
@@ -600,8 +600,10 @@ static void decode_gains(AMRContext *p, const AMRNBSubframe *amr_subframe,
                          float *fixed_gain_factor)
 {
     if (mode == MODE_12k2 || mode == MODE_7k95) {
-        p->pitch_gain[4]   = qua_gain_pit [amr_subframe->p_gain    ] / 16384.0;
-        *fixed_gain_factor = qua_gain_code[amr_subframe->fixed_gain] / 2048.0;
+        p->pitch_gain[4]   = qua_gain_pit [amr_subframe->p_gain    ]
+            * (1.0 / 16384.0);
+        *fixed_gain_factor = qua_gain_code[amr_subframe->fixed_gain]
+            * (1.0 /  2048.0);
     } else {
         const uint16_t *gains;
 
@@ -614,8 +616,8 @@ static void decode_gains(AMRContext *p, const AMRNBSubframe *amr_subframe,
             gains = gains_MODE_4k75[(p->frame.subframe[subframe & 2].p_gain << 1) + (subframe & 1)];
         }
 
-        p->pitch_gain[4]   = gains[0] / 16384.0;
-        *fixed_gain_factor = gains[1] / 4096.0;
+        p->pitch_gain[4]   = gains[0] * (1.0 / 16384.0);
+        *fixed_gain_factor = gains[1] * (1.0 /  4096.0);
     }
 }
 
