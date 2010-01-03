78f4891559e6753875845877ecc4884e1fb71dc3
vitor <vitor@66570020-b32e-0410-962d-e44e8f9afd43>
2010-01-03 20:48:04 +0000

    Whitespace cosmetics

--- amr/amrnbdec.c
+++ amr/amrnbdec.c
@@ -105,7 +105,7 @@ static void weighted_vector_sumd(double *out, const double *in_a,
 {
     int i;
 
-    for(i = 0; i < length; i++)
+    for (i = 0; i < length; i++)
         out[i] = weight_coeff_a * in_a[i]
                + weight_coeff_b * in_b[i];
 }
