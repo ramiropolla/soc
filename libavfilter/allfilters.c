8ec9eada2967a23b8e487ad532de1f8d105147c9
stefano <stefano@66570020-b32e-0410-962d-e44e8f9afd43>
2010-02-19 00:00:18 +0000

    Add pad filter.
    The filter is currently discussed in ffmpeg-devel, but some problems
    need to be fixed before it can be committed to the main repo.
    
    See the thread:
    Subject: [FFmpeg-devel] [PATCH] Add pad filter
    Date: Sun, 8 Nov 2009 21:50:13 +0100
    http://thread.gmane.org/gmane.comp.video.ffmpeg.devel/99314/

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -44,6 +44,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER(NOFORMAT,noformat,vf);
     REGISTER_FILTER(NULL,null,vf);
     REGISTER_FILTER(OVERLAY,overlay,vf);
+    REGISTER_FILTER(PAD,pad,vf);
     REGISTER_FILTER(ROTATE,rotate,vf);
     REGISTER_FILTER(SCALE,scale,vf);
     REGISTER_FILTER(SETPTS,setpts,vf);
