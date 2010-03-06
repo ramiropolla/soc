ae029aa52328fbed8e40dc9bc0abbeb023ddc978
stefano <stefano@66570020-b32e-0410-962d-e44e8f9afd43>
2010-03-06 14:29:13 +0000

    Update to FFmpeg r22232 / libswscale r30856, and drop the ffplay patch, which is now integrated into the main FFmpeg repository.

--- libavfilter/checkout.sh
+++ libavfilter/checkout.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 echo "checking out pristine ffmpeg"
-svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r22063
+svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r22232
 
 echo "downloading the corresponding version of swscale"
 cd ffmpeg/libswscale
-svn up -r30730
+svn up -r30856
 cd ../..
 
 echo "patching ffmpeg"
