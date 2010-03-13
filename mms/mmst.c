668a51066dd1dd8eb393e702af2b607418ac07f3
spyfeng <spyfeng@66570020-b32e-0410-962d-e44e8f9afd43>
2010-03-13 12:22:27 +0000

    fix a potential buffer overread according to Ronald S. Bultje's comment.

--- mms/mmst.c
+++ mms/mmst.c
@@ -869,7 +869,7 @@ static int asf_header_parser(MMSContext *mms)
         uint64_t chunksize = AV_RL64(p + sizeof(ff_asf_guid));
         if (!memcmp(p, ff_asf_file_header, sizeof(ff_asf_guid))) {
             /* read packet size */
-            if (end - p > sizeof(ff_asf_guid) * 2 + 64) {
+            if (end - p > sizeof(ff_asf_guid) * 2 + 68) {
                 mms->asf_packet_len = AV_RL32(p + sizeof(ff_asf_guid) * 2 + 64);
             }
         } else if (!memcmp(p, ff_asf_stream_header, sizeof(ff_asf_guid))) {
