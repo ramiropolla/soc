d58df1c42b9006d3cdef13fcb50b9c97d2e17db4
stefano <stefano@66570020-b32e-0410-962d-e44e8f9afd43>
2010-03-18 00:12:30 +0000

    Update to FFmpeg r22589 / libswscale r30869.

--- libavfilter/checkout.sh
+++ libavfilter/checkout.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "checking out pristine ffmpeg"
-svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r22508
+svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk/ ffmpeg -r22589
 
 echo "downloading the corresponding version of swscale"
 cd ffmpeg/libswscale
