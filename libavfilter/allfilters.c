4f24c31f4053d35dec65b615e5324cde05d033f9
stefano <stefano@66570020-b32e-0410-962d-e44e8f9afd43>
2010-05-16 15:39:13 +0000

    Port VHOOK drawtext filter. Configure with --enable-freetype to compile it in.
    Patch by S.N. Hemanth Meenakshisundaram |smeenaks|ucsd|edu|.

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -37,6 +37,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER (ASPECT,      aspect,      vf);
     REGISTER_FILTER (CROP,        crop,        vf);
     REGISTER_FILTER (DRAWBOX,     drawbox,     vf);
+    REGISTER_FILTER (DRAWTEXT,    drawtext,    vf);
     REGISTER_FILTER (FIFO,        fifo,        vf);
     REGISTER_FILTER (FORMAT,      format,      vf);
     REGISTER_FILTER (FPS,         fps,         vf);
